0f8f8a82bbdfae3acc754bfd1a0b31cc610bf19f
taeyoung24 <jtyoung24@naver.com>
2026-03-22 17:35:03 +0900

    ci: add prisma-studio deleting process

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,5 +3,6 @@
 echo "Next.js 애플리케이션을 완전히 종료합니다..."
 
 pm2 delete bono-open-class
+pm2 delete prisma-studio
 
-echo "애플리케이션이 완전히 종료되고 PM2 목록에서 삭제되었습니다."
+echo "애플리케이션과 DB 스튜디오가 PM2 목록에서 깔끔하게 삭제되었습니다."
