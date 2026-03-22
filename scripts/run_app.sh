7e5e275bc20dc5166610fae24cfa86e10a69a990
taeyoung24 <jtyoung24@naver.com>
2026-03-22 18:21:31 +0900

    fix: update prisma studio start command

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -11,7 +11,7 @@ PORT=5033 pm2 start pnpm --name "bono-open-class" -- start
 
 echo "Prisma Studio 실행 중 (백그라운드)..."
 # --host 0.0.0.0 옵션을 추가하여 Tailscale IP로 접속 가능하게 설정
-pm2 start pnpm --name "prisma-studio" -- prisma studio --browser none --host 0.0.0.0
+pm2 start "pnpm prisma studio --port 5555 --browser none --hostname 0.0.0.0" --name "prisma-studio"
 
 echo "Next.js 애플리케이션이 PM2 백그라운드에서 시작되었습니다!"
 echo "상태를 보려면 'pm2 status' 또는 실시간 로그를 보려면 'pm2 logs bono-open-class'를 입력하세요."
